8e3d479fe1ede8f143a528e65ac38bd623cc3c3f
vtu29168 <vtu29168@veltech.edu.in>
2026-09-17 14:37:43 +0530

    Time: 4 ms (73.48%), Space: 45.3 MB (49.72%) - LeetHub

---
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> nextGreaterMap = new HashMap<>();
+        Stack<Integer> stack = new Stack<>();
+        
+        // Step 1: Precompute next greater elements for all elements in nums2
+        for (int num : nums2) {
+            while (!stack.isEmpty() && stack.peek() < num) {
+                nextGreaterMap.put(stack.pop(), num);
+            }
+            stack.push(num);
+        }
+        
+        // Step 2: Build the result array for nums1 using the map
+        int[] ans = new int[nums1.length];
+        for (int i = 0; i < nums1.length; i++) {
+            ans[i] = nextGreaterMap.getOrDefault(nums1[i], -1);
+        }
+        
+        return ans;
+    }
+}
